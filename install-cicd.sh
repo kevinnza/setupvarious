e509f2f1fd6f8147f4f43a4762e57fd66d01728a
Kevin Naidu <kevin@blockwerx.com>
2018-10-21 22:54:07 +0200

    wip

--- install-cicd.sh
+++ install-cicd.sh
@@ -58,7 +58,22 @@ sudo apt install build-essential -y
 
 
 
-# install Java 
+# install .NET Core
+echo ""
+echo "******************************"
+echo "*  Installing .NET Core 2.1  *"
+echo "******************************"
+echo ""
+
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get install apt-transport-https -y
+sudo apt-get update -y
+sudo apt-get install dotnet-sdk-2.1 -y
+
+
+
+# install Java (Open JDK)
 echo ""
 echo "******************************"
 echo "*  Installing Java Open JDK  *"
@@ -67,6 +82,32 @@ echo ""
 
 sudo apt update -y
 sudo apt install openjdk-8-jdk -y
+
+
+
+# install Android SDK
+echo ""
+echo "******************************"
+echo "*  Installing Android SDK    *"
+echo "******************************"
+echo ""
+
+# NOTE: if the zip link doesn't exist anymore then
+#       - Go to https://developer.android.com/studio/#downloads 
+#       - Find the "Command Line Tools only" section
+#       - Locate the SDK tools package for Linux and download the zip file.
+#       - Goto your browser's download history and locate the link from there
+#       - replace the value of ANDROID_SDK_DOWNLOAD_URI with the link you found
+#       - replace the value of ANDROID_SDK_ZIP_FILE with the name of the zip file you found
+ANDROID_SDK_DOWNLOAD_URI="https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
+ANDROID_SDK_ZIP_FILE="sdk-tools-linux-4333796.zip"
+wget $ANDROID_SDK_DOWNLOAD_URI
+mkdir $HOME/Android && mkdir $HOME/Android/Sdk
+unzip $ANDROID_SDK_ZIP_FILE -d $HOME/Android/Sdk
+# Set ANDROID_HOME system environment variables by creating a new .sh file in /etc/profile.d:
+sudo bash -c "echo -e \"export ANDROID_HOME=$HOME/Android/Sdk\nexport PATH=$PATH:$HOME/Android/Sdk/tools:$HOME/Android/Sdk/platform-tools \" > /etc/profile.d/android-sdk-path.sh"
+# download the Android sdks required for NativeScript
+sudo $HOME/Android/Sdk/tools/bin/sdkmanager "tools" "emulator" "platform-tools" "platforms;android-25" "build-tools;27.0.3" "extras;android;m2repository" "extras;google;m2repository"
 
 
 
@@ -108,6 +149,7 @@ sudo ufw allow 8080
 
 # wait for a bit
 sleep 5
+
 
 
 # show the initial admin password
