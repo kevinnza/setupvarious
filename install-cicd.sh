aef0fd9a4ff010a95488c03922d701a11254b9cd
Kevin Naidu <kevin@blockwerx.com>
2018-10-21 20:53:19 +0200

    wip

--- install-cicd.sh
+++ install-cicd.sh
@@ -1,15 +1,16 @@
 # install Java 
 echo "Installing Java Open JDK"
 
-sudo apt update && sudo apt install openjdk-8-jdk
+sudo -y apt update
+sudo -y apt install openjdk-8-jdk
 
 # install Jenkins
 echo "Installing Jenkins"
 
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add - &&
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' &&
-sudo -y apt update &&
-sudo -y apt install jenkins &&
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo -y apt update
+sudo -y apt install jenkins
 sudo systemctl start jenkins
 
 # enabled firewall in case it is not enabled
