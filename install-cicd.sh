c69c5bf95e2e4dcef68e496a44820e8a9f7a943a
Kevin Naidu <kevin@blockwerx.com>
2018-10-21 22:22:01 +0200

    wip

--- install-cicd.sh
+++ install-cicd.sh
@@ -46,7 +46,7 @@ echo "************************"
 
 cd ~
 curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh -y
+sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
 sudo apt install build-essential -y
 
