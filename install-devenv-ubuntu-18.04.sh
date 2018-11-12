c3c4b68606465c84d434d9bee9461850ee5045f4
kevinnza <kevinnza@gmail.com>
2018-11-12 17:02:02 +0200

    wip

--- install-devenv-ubuntu-18.04.sh
+++ install-devenv-ubuntu-18.04.sh
@@ -58,7 +58,11 @@ cd ~
 
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh
 sudo bash install_nvm.sh
-source ~/.profile
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 nvm install 8.10.0
 
 # install .NET Core
